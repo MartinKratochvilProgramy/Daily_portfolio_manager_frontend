12be3b27a7adec63b6417765f9d5a42b20a3de17
MartinKratochvilProgramy <martvil96@gmail.com>
2022-10-13 15:53:21 +0200

    removed rounded on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,19 +14,19 @@ export default function Navbar({ active }) {
         navigate("/");
     }
 
-    const activeStyles = "block p-2 sm:p-4 text-white bg-blue-700 rounded bg-transparent text-blue-700 dark:text-white";
-    const nonActiveStyles = "block p-2 sm:p-4 text-gray-700 rounded hover:bg-gray-100 hover:bg-transparent hover:text-blue-700 dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:hover:bg-transparent dark:border-gray-700";
+    const activeStyles = "block p-2 sm:p-4 text-white bg-blue-700 bg-transparent text-blue-700 dark:text-white";
+    const nonActiveStyles = "block p-2 sm:p-4 text-gray-700 hover:bg-gray-100 hover:bg-transparent hover:text-blue-700 dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:hover:bg-transparent dark:border-gray-700";
 
   return (
     <div>
-        <nav className="bg-white border-gray-200 px-2 sm:px-4 rounded dark:bg-gray-900">
+        <nav className="bg-white border-gray-200 px-2 sm:px-4 dark:bg-gray-900">
             <div className="flex justify-between items-center w-auto order-1" id="navbar-search">
 
                 <div className='text-white font-medium hidden sm:block'>
                     {credentials.username}
                 </div>
 
-                <ul className="flex flex-row justify-end  items-center p-1 rounded-lg border-gray-100 space-x-0 md:space-x-4 mt-0 text-sm font-medium border-0 bg-white dark:bg-gray-900 dark:border-gray-700">
+                <ul className="flex flex-row justify-end  items-center p-1-lg border-gray-100 space-x-0 md:space-x-4 mt-0 text-sm font-medium border-0 bg-white dark:bg-gray-900 dark:border-gray-700">
                     <li>
                         <a href="/stocks" className={active === "stocks" ? activeStyles : nonActiveStyles}>
                             Stocks
@@ -50,7 +50,7 @@ export default function Navbar({ active }) {
                     <li>
                         <button
                             type="submit"
-                            className="flex flex-row px-5 py-2 text-blue-600 border-solid border-blue-600 border-[1px] bg-white font-medium text-sm leading-snug uppercase rounded whitespace-nowrap shadow-md hover:bg-blue-700 hover:text-white hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                            className="flex flex-row px-5 py-2 text-blue-600 border-solid border-blue-600 border-[1px] bg-white font-medium text-sm leading-snug uppercase whitespace-nowrap shadow-md hover:bg-blue-700 hover:text-white hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
                             onClick={logout}
                             >
                             Logout
