d9c5bbdfa6ff1d777bbdcf5f6c14c0244ea5b07d
MartinKratochvilProgramy <martvil96@gmail.com>
2022-10-14 20:28:19 +0200

    better dollar sign

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,47 +18,45 @@ export default function Navbar({ active }) {
     const nonActiveStyles = "block p-2 sm:p-4 text-gray-700 hover:bg-gray-100 hover:bg-transparent hover:text-blue-700 dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:hover:bg-transparent dark:border-gray-700";
 
   return (
-    <div>
-        <nav className="bg-white border-gray-200 px-2 sm:px-4 dark:bg-gray-900">
-            <div className="flex justify-between items-center w-auto order-1" id="navbar-search">
+    <nav className="bg-white border-gray-200 px-2 sm:px-4 dark:bg-gray-900">
+        <div className="flex justify-between items-center w-auto order-1" id="navbar-search">
 
-                <div className='text-white font-medium hidden sm:block'>
-                    {credentials.username}
-                </div>
-
-                <ul className="flex flex-row justify-end  items-center p-1-lg border-gray-100 space-x-0 md:space-x-4 mt-0 text-sm font-medium border-0 bg-white dark:bg-gray-900 dark:border-gray-700">
-                    <li>
-                        <a href="/stocks" className={active === "stocks" ? activeStyles : nonActiveStyles}>
-                            Stocks
-                        </a>
-                    </li>
-                    <li>
-                        <a href="charts" className={active === "charts" ? activeStyles : nonActiveStyles}>
-                            Charts
-                        </a>
-                    </li>
-                    <li>
-                        <a href="investments" className={active === "investments" ? activeStyles : nonActiveStyles}>
-                            Investments
-                        </a>
-                    </li>
-                    <li>
-                        <a href="about" className={active === "about" ? activeStyles : nonActiveStyles}>
-                            About
-                        </a>
-                    </li>
-                    <li>
-                        <button
-                            type="submit"
-                            className="flex flex-row px-5 py-2 text-blue-600 rounded border-solid border-blue-600 border-[1px] bg-white font-medium text-sm leading-snug uppercase whitespace-nowrap shadow-md hover:bg-blue-700 hover:text-white hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-                            onClick={logout}
-                            >
-                            Logout
-                        </button>
-                    </li>
-                </ul>
+            <div className='text-white font-medium hidden sm:block'>
+                {credentials.username}
             </div>
-        </nav>
-    </div>
+
+            <ul className="flex flex-row justify-end  items-center p-1-lg border-gray-100 space-x-0 md:space-x-4 mt-0 text-sm font-medium border-0 bg-white dark:bg-gray-900 dark:border-gray-700">
+                <li>
+                    <a href="/stocks" className={active === "stocks" ? activeStyles : nonActiveStyles}>
+                        Stocks
+                    </a>
+                </li>
+                <li>
+                    <a href="charts" className={active === "charts" ? activeStyles : nonActiveStyles}>
+                        Charts
+                    </a>
+                </li>
+                <li>
+                    <a href="investments" className={active === "investments" ? activeStyles : nonActiveStyles}>
+                        Investments
+                    </a>
+                </li>
+                <li>
+                    <a href="about" className={active === "about" ? activeStyles : nonActiveStyles}>
+                        About
+                    </a>
+                </li>
+                <li>
+                    <button
+                        type="submit"
+                        className="flex flex-row px-5 py-2 text-blue-600 rounded border-solid border-blue-600 border-[1px] bg-white font-medium text-sm leading-snug uppercase whitespace-nowrap shadow-md hover:bg-blue-700 hover:text-white hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                        onClick={logout}
+                        >
+                        Logout
+                    </button>
+                </li>
+            </ul>
+        </div>
+    </nav>
   )
 }
