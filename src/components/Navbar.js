973a956212c7828e6decb91fffd429b7b8af95f3
MartinKratochvilProgramy <martvil96@gmail.com>
2022-10-15 16:59:04 +0200

    navbar items flex-col-reverse on mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -36,9 +36,9 @@ export default function Navbar({ active }) {
 
   return (
     <nav className="border-gray-200 px-0 sm:px-4 bg-gray-900">
-        <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center w-auto order-1" id="navbar-search">
+        <div className="flex flex-col-reverse sm:flex-row justify-between items-start sm:items-center w-auto order-1" id="navbar-search">
 
-            <div className='flex w-full sm:w-auto py-1 px-2 justify-between items-center text-white space-x-4'>
+            <div className='flex w-full sm:w-auto py-1 px-2 justify-start items-center text-white space-x-4'>
                 <div className='pb-[2px]'>
                     {credentials.username}
                 </div>
