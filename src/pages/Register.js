5c983cf1cb53e308c947214576994b2a7543a7c8
MartinKratochvilProgramy <martvil96@gmail.com>
2022-10-20 17:39:51 +0200

    added form components into /register

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import {  useNavigate } from 'react-router-dom';
 import { CredentialsContext, CurrencyContext } from '../App';
 import { handleErrors } from './Login';
 import RegisterForm from '../components/RegisterForm';
