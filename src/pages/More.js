252d1834e9637a93a96753184c02f9a0ee18f6b7
martin.kratochvil <martvil96@gmail.com>
2022-11-01 08:44:39 +0100

    added Find out more... page

--- src/pages/More.js
+++ src/pages/More.js
@@ -14,24 +14,24 @@ export default function More() {
             search for corresponding tickers (eg. 'AAPL' or 'MSFT'). When creating your account, you can choose from three currencies, in which to display financial data : USD, EUR or CZK - currency
             conversion rates are fetched from the Yahoo API.
         </p>
-        <img src={require("../img/stocks.png")} alt=""></img>
+        <img src={require("../img/stocks.png")} alt="stock input example"></img>
 
         <p className='max-w-[60%] text-xl'>
             Because the amounts of each stock in your portfolio may change, every day the server looks for previous close price for each stock in your portfolio and updates current net-worth. 
             Total net worth and relative change is displayed.
         </p>
-        <img className='w-10/12' src={require("../img/net_worth.png")} alt=""></img>
-        <img className='w-10/12' src={require("../img/relative_change.png")} alt=""></img>
+        <img className='w-10/12' src={require("../img/net_worth.png")} alt="time-series of total net worth"></img>
+        <img className='w-10/12' src={require("../img/relative_change.png")} alt="time-series of relative % change"></img>
 
         <p className='max-w-[60%] text-xl'>
             To manage your risk more easily, pie chart of % valuation of each stock is displayed.
         </p>
-        <img src={require("../img/pie.png")} alt=""></img>
+        <img src={require("../img/pie.png")} alt="pi chart of all users stocks"></img>
 
         <p className='max-w-[60%] text-xl'>
             To get a rough idea about how much you invest and what is your discipline when investing, the Investments section displays total invested amount where the amount each day you bought a new stock.
         </p>
-        <img className='w-10/12' src={require("../img/investment_history.png")} alt=""></img>
+        <img className='w-10/12' src={require("../img/investment_history.png")} alt="time-series of amount invested in portfolio"></img>
 
         <p className='max-w-[60%] text-xl'>
             This app is intended for the 'Bogglehead' type investor who likes to invest regularly over longer periods of time.
