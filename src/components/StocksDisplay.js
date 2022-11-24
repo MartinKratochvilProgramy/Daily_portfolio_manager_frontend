699777dce28b51a609845c2677364be24d22befe
MartinKratochvilProgramy <martvil96@gmail.com>
2022-11-24 14:35:48 +0100

    added search

--- src/components/StocksDisplay.js
+++ src/components/StocksDisplay.js
@@ -40,7 +40,7 @@ export default function Stocks({ stocks, setStocks, setError }) {
             onChange={(e) => {setSearchKey(e.target.value)}}
             className='w-4/12 sm:w-3/12 lg:w-2/12 px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none'
             type="text" 
-            placeholder='Filter...' />
+            placeholder='Search...' />
         </div>
         {stocks.map((stock) => {
           if (stock.ticker.includes(searchKey.toUpperCase())) {
