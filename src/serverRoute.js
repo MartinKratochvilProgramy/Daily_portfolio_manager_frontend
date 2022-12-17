6a188aa660377eb5d9e1154d7bf47428f32852d0
MartinKratochvilProgramy <martvil96@gmail.com>
2022-12-17 12:33:08 +0100

    cookie branch

--- src/serverRoute.js
+++ src/serverRoute.js
@@ -1,4 +1,4 @@
-export const serverRoute = "https://daily-portfolio-management.cyclic.app"
+export const serverRoute = "//localhost:4000";
 
 // https://dailyportfoliomanager.herokuapp.com
 // https://daily-portfolio-management.cyclic.app
